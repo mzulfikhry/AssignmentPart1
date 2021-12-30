c93450085a321974b32cd4e69689bff033353a68
mzulfikhry <mzulfikhry@gmail.com>
2021-12-30 22:16:56 +0800

    My first commit

--- lib/MainScreen/body.dart
+++ lib/MainScreen/body.dart
@@ -43,232 +43,101 @@ class _MainScreenBodyState extends State<MainScreenBody>
               ],
             ),
             Container(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: TabBar(
-                  controller: _tabController,
-                  isScrollable: true,
-                  labelPadding: const EdgeInsets.only(left: 30, right: 30),
-                  labelColor: Colors.black,
-                  unselectedLabelColor: Colors.grey,
-                  tabs: [
-                    Tab(text: "All"),
-                    Tab(text: "Camera"),
-                    Tab(text: "Light"),
-                    Tab(text: "Aircond"),
-                    Tab(text: "Gate"),
+                width: double.maxFinite,
+                height: 700,
+                child: GridView.count(
+                  primary: false,
+                  padding: const EdgeInsets.all(20),
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  crossAxisCount: 2,
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: CustomCard(
+                        size: size,
+                        icon: Icon(
+                          Icons.lightbulb_outline,
+                          size: 55,
+                          color: Colors.grey.shade400,
+                        ),
+                        title: "MAIN LIGHT",
+                        statusOn: "OPEN",
+                        statusOff: "LOCKED",
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: CustomCard(
+                        size: size,
+                        icon: Icon(
+                          Icons.lightbulb_outline_sharp,
+                          size: 55,
+                          color: Colors.grey.shade400,
+                        ),
+                        title: "MAIN SWITCH",
+                        statusOn: "ON",
+                        statusOff: "OFF",
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: CustomCard(
+                        size: size,
+                        icon: Icon(
+                          Icons.ac_unit,
+                          size: 55,
+                          color: Colors.grey.shade400,
+                        ),
+                        title: "AIRCOND",
+                        statusOn: "ON",
+                        statusOff: "OFF",
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: CustomCard(
+                        size: size,
+                        icon: Icon(
+                          Icons.home_outlined,
+                          size: 55,
+                          color: Colors.grey.shade400,
+                        ),
+                        title: "GATE",
+                        statusOn: "OPEN",
+                        statusOff: "LOCKED",
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: CustomCard(
+                        size: size,
+                        icon: Icon(
+                          Icons.camera_indoor_outlined,
+                          size: 55,
+                          color: Colors.grey.shade400,
+                        ),
+                        title: "CAMERA",
+                        statusOn: "ON",
+                        statusOff: "OFF",
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: CustomCard(
+                        size: size,
+                        icon: Icon(
+                          Icons.alarm,
+                          size: 55,
+                          color: Colors.grey.shade400,
+                        ),
+                        title: "DOOR ALARM",
+                        statusOn: "ON",
+                        statusOff: "OFF",
+                      ),
+                    ),
                   ],
-                ),
-              ),
-            ),
-            Container(
-              width: double.maxFinite,
-              height: 700,
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  GridView.count(
-                    primary: false,
-                    padding: const EdgeInsets.all(20),
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    crossAxisCount: 2,
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: CustomCard(
-                          size: size,
-                          icon: Icon(
-                            Icons.lightbulb_outline,
-                            size: 55,
-                            color: Colors.grey.shade400,
-                          ),
-                          title: "MAIN LIGHT",
-                          statusOn: "OPEN",
-                          statusOff: "LOCKED",
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: CustomCard(
-                          size: size,
-                          icon: Icon(
-                            Icons.lightbulb_outline_sharp,
-                            size: 55,
-                            color: Colors.grey.shade400,
-                          ),
-                          title: "MAIN SWITCH",
-                          statusOn: "ON",
-                          statusOff: "OFF",
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: CustomCard(
-                          size: size,
-                          icon: Icon(
-                            Icons.ac_unit,
-                            size: 55,
-                            color: Colors.grey.shade400,
-                          ),
-                          title: "AIRCOND",
-                          statusOn: "ON",
-                          statusOff: "OFF",
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: CustomCard(
-                          size: size,
-                          icon: Icon(
-                            Icons.home_outlined,
-                            size: 55,
-                            color: Colors.grey.shade400,
-                          ),
-                          title: "GATE",
-                          statusOn: "OPEN",
-                          statusOff: "LOCKED",
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: CustomCard(
-                          size: size,
-                          icon: Icon(
-                            Icons.camera_indoor_outlined,
-                            size: 55,
-                            color: Colors.grey.shade400,
-                          ),
-                          title: "CAMERA",
-                          statusOn: "ON",
-                          statusOff: "OFF",
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: CustomCard(
-                          size: size,
-                          icon: Icon(
-                            Icons.alarm,
-                            size: 55,
-                            color: Colors.grey.shade400,
-                          ),
-                          title: "DOOR ALARM",
-                          statusOn: "ON",
-                          statusOff: "OFF",
-                        ),
-                      ),
-                    ],
-                  ),
-                  GridView.count(
-                    primary: false,
-                    padding: const EdgeInsets.all(20),
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    crossAxisCount: 2,
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: CustomCard(
-                          size: size,
-                          icon: Icon(
-                            Icons.lightbulb_outline_sharp,
-                            size: 55,
-                            color: Colors.grey.shade400,
-                          ),
-                          title: "MAIN SWITCH",
-                          statusOn: "ON",
-                          statusOff: "OFF",
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: CustomCard(
-                          size: size,
-                          icon: Icon(
-                            Icons.lightbulb_outline,
-                            size: 55,
-                            color: Colors.grey.shade400,
-                          ),
-                          title: "MAIN LIGHT",
-                          statusOn: "OPEN",
-                          statusOff: "LOCKED",
-                        ),
-                      ),
-                    ],
-                  ),
-                  GridView.count(
-                    primary: false,
-                    padding: const EdgeInsets.all(20),
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    crossAxisCount: 2,
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: CustomCard(
-                          size: size,
-                          icon: Icon(
-                            Icons.ac_unit,
-                            size: 55,
-                            color: Colors.grey.shade400,
-                          ),
-                          title: "AIRCOND",
-                          statusOn: "ON",
-                          statusOff: "OFF",
-                        ),
-                      ),
-                    ],
-                  ),
-                  GridView.count(
-                    primary: false,
-                    padding: const EdgeInsets.all(20),
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    crossAxisCount: 2,
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: CustomCard(
-                          size: size,
-                          icon: Icon(
-                            Icons.ac_unit,
-                            size: 55,
-                            color: Colors.grey.shade400,
-                          ),
-                          title: "AIRCOND",
-                          statusOn: "ON",
-                          statusOff: "OFF",
-                        ),
-                      ),
-                    ],
-                  ),
-                  GridView.count(
-                    primary: false,
-                    padding: const EdgeInsets.all(20),
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    crossAxisCount: 2,
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: CustomCard(
-                          size: size,
-                          icon: Icon(
-                            Icons.home_outlined,
-                            size: 55,
-                            color: Colors.grey.shade400,
-                          ),
-                          title: "GATE",
-                          statusOn: "OPEN",
-                          statusOff: "LOCKED",
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+                )),
           ],
         ));
   }
